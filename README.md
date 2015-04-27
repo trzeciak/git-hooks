7d1d99aa00abd2e148a0705ab3978d9fd0d3a51d
Damian Trzeciak <DamianT@vp.pl>
2015-04-27 23:17:08 +0200

    Add sym. link, and test

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # git-hooks
 http://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks
-1
-2
+
+Add symbolik link:
+
+ln -s -f dt-pre-commit-script.sh .git/hooks/pre-commit
+chmod +x dt-pre-commit-script.sh
+
--- dt-pre-commit-script.sh
+++ dt-pre-commit-script.sh
@@ -4,9 +4,26 @@
 FILE="dt-log.txt"
 LOG=`date "+DATE-%Y-%m-%d_TIME-%H-%M-%S.txt"`
 
+# --------------------------------
+
 touch "$FILE"
 NUM=$(($(cat "$FILE" | wc -l)+1))
+MSG=" $NUM:\t $LOG\t\t`basename $0` $@"
 
-echo -e " $NUM:\t $LOG\t\t`basename $0` $@"
-echo -e " $NUM:\t $LOG\t\t`basename $0` $@" >> "$FILE"
+# --------------------------------
+
+
+# --------------------------------
+
+echo ""
+echo ""
+echo "Info: Pre-commit."
+echo ""
+echo -e "$MSG"
+echo -e "$MSG" >> "$FILE"
+git add "$FILE"
+echo ""
+echo ""
+
+# --------------------------------
 
